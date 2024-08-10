f4207a77010023e0e02b36de5689f8606d48e387
Clément Hallet <clement@challet.eu>
2024-08-10 14:47:11 +0200

    handle map bounds changes

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useLayoutEffect, useState } from "react";
 import { EvmTorus } from "../utils/leaflet/evmWorld";
-import { Icon, LeafletEvent, latLng, latLngBounds } from "leaflet";
+import { Icon, LatLngBounds } from "leaflet";
 import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import iconUrl from "leaflet/dist/images/marker-icon.png";
 import shadowUrl from "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/leaflet.css";
 import type { NextPage } from "next";
-import { MapContainer, Marker, ScaleControl, TileLayer, useMapEvent } from "react-leaflet";
+import { MapContainer, Marker, ScaleControl, TileLayer, useMap, useMapEvent } from "react-leaflet";
 import useSWR, { Fetcher } from "swr";
 
 const icon = new Icon.Default({
@@ -23,43 +23,27 @@ interface EVMObject {
   lng: number;
 }
 
-const MoveHandler: FunctionComponent<{ onMove: (event: LeafletEvent) => void }> = ({ onMove }) => {
-  useMapEvent("move", onMove);
-  return null;
-};
-
-const dataFetch: Fetcher<EVMObject[], string> = async () => {
-  const response = await fetch("http://localhost:3001/objects");
-  const data = await response.json();
-  return data;
-};
-
 const Home: NextPage = () => {
-  const [mapBounds, setMapBounds] = useState(latLngBounds(latLng(0, 0), latLng(0, 0)));
-  const { data, isLoading } = useSWR("dummy", dataFetch);
-
-  const onMove = (event: LeafletEvent) => {
-    setMapBounds(event.target.getBounds());
-  };
+  const [mapBounds, setMapBounds] = useState<LatLngBounds | null>(null);
 
   return (
     <>
       <div className="stats shadow">
         <div className="stat">
           <div className="stat-title">West</div>
-          <div className="stat-value">{mapBounds.getWest()}</div>
+          <div className="stat-value">{mapBounds ? mapBounds.getWest() : null}</div>
         </div>
         <div className="stat">
           <div className="stat-title">North</div>
-          <div className="stat-value">{mapBounds.getNorth()}</div>
+          <div className="stat-value">{mapBounds ? mapBounds.getNorth() : null}</div>
         </div>
         <div className="stat">
           <div className="stat-title">South</div>
-          <div className="stat-value">{mapBounds.getSouth()}</div>
+          <div className="stat-value">{mapBounds ? mapBounds.getSouth() : null}</div>
         </div>
         <div className="stat">
           <div className="stat-title">East</div>
-          <div className="stat-value">{mapBounds.getEast()}</div>
+          <div className="stat-value">{mapBounds ? mapBounds.getEast() : null}</div>
         </div>
       </div>
 
@@ -75,17 +59,39 @@ const Home: NextPage = () => {
         >
           <TileLayer url="http://localhost:3001/tiles/{z}/{x}/{y}.png" noWrap={false} />
           <ScaleControl />
-          <Marker position={[0, 0]} icon={icon} />
-          <MoveHandler onMove={onMove} />
-          {!isLoading &&
-            data &&
-            data.map(d => {
-              return <Marker position={[d.lat, d.lng]} icon={icon} key={d.id} />;
-            })}
+          <MoveHandler onBoundsChange={setMapBounds} />
+          {mapBounds && <Markers bounds={mapBounds} />}
         </MapContainer>
       </div>
     </>
   );
+};
+
+const MoveHandler: FunctionComponent<{ onBoundsChange: (bounds: LatLngBounds) => void }> = ({ onBoundsChange }) => {
+  const map = useMap();
+  useLayoutEffect(() => onBoundsChange(map.getBounds()), [map, onBoundsChange]);
+  useMapEvent("move", event => onBoundsChange(event.target.getBounds()));
+  return null;
+};
+
+// TODO : better fetch by area batches
+const dataFetch: Fetcher<EVMObject[], LatLngBounds> = async bounds => {
+  const response = await fetch(`http://localhost:3001/objects?bounds=${bounds.toBBoxString()}`);
+  const data = await response.json();
+  return data;
+};
+
+const Markers: FunctionComponent<{ bounds: LatLngBounds }> = ({ bounds }) => {
+  const map = useMap();
+  const { data, isLoading } = useSWR(EvmTorus.constraintsLatLngBounds(map.wrapLatLngBounds(bounds)), dataFetch);
+
+  if (!isLoading && data) {
+    return data.map(d => {
+      return <Marker position={[d.lat, d.lng]} icon={icon} key={d.id} />;
+    });
+  } else {
+    return null;
+  }
 };
 
 export default Home;
--- packages/nextjs/utils/leaflet/evmWorld.ts
+++ packages/nextjs/utils/leaflet/evmWorld.ts
@@ -1,11 +1,11 @@
 "use client";
 
-import { Bounds, CRS, LatLng, Point, Projection, Util, transformation } from "leaflet";
+import { Bounds, CRS, LatLng, LatLngBounds, Point, Projection, Util, transformation } from "leaflet";
 
 // The native Javascript min and max integers are respectively -(2^53 – 1) and (2^53 – 1)
 // None map computations should result in over or underflow
 // Keep it as close as possible to the max
-export const MAX_SAFE_COORDINATES = Math.pow(2, 49); //  562949953421312
+export const MAX_SAFE_COORDINATES = Math.pow(2, 49); //   562949953421312
 export const MIN_SAFE_COORDINATES = -Math.pow(2, 49); // -562949953421312
 
 // The range of the world is 2^50 (-2^49 to 2^49)
@@ -15,10 +15,17 @@ export const MIN_SAFE_COORDINATES = -Math.pow(2, 49); // -562949953421312
 export const ISO_ZOOM = 42;
 const BASE_SCALE = 1 / Math.pow(2, ISO_ZOOM);
 
+// Bounds in "geographical" coordinates
+const WORLD_BOUNDS = new LatLngBounds(
+  new LatLng(MAX_SAFE_COORDINATES, MIN_SAFE_COORDINATES),
+  new LatLng(MIN_SAFE_COORDINATES, MAX_SAFE_COORDINATES),
+);
+
+// Coordinates from the EVM world are between -(2^79 - 1) and (2^79 - 1)
+// Coordinates should be down-scaled by 2^(79-50)
+// Or we need to rewrite the leaflet objects to handle BigInts
 export const EvmLonLat = Util.extend({}, Projection.LonLat, {
-  // Coordinates from the EVM world are between -(2^79 - 1) and (2^79 - 1)
-  // Coordinates should be down-scaled by 2^(79-50)
-  // Or we need to rewrite the leaflet objects to handle BigInts
+  // Bounds in CRS coordinates (~ pixels at scale 1)
   bounds: new Bounds([MIN_SAFE_COORDINATES, MIN_SAFE_COORDINATES], [MAX_SAFE_COORDINATES, MAX_SAFE_COORDINATES]),
   project(latlng: LatLng) {
     return new Point(latlng.lng - MIN_SAFE_COORDINATES, latlng.lat - MIN_SAFE_COORDINATES);
@@ -28,16 +35,40 @@ export const EvmLonLat = Util.extend({}, Projection.LonLat, {
   },
 });
 
-export const EvmTorus = Util.extend({}, CRS.Simple, {
-  wrapLng: [MIN_SAFE_COORDINATES, MAX_SAFE_COORDINATES],
-  wrapLat: [MIN_SAFE_COORDINATES, MAX_SAFE_COORDINATES],
-  zoom(scale: number) {
-    Math.log(scale / BASE_SCALE) / Math.LN2;
+export const EvmTorus: CRS & { constraintsLatLngBounds: (bounds: LatLngBounds) => LatLngBounds | null } = Util.extend(
+  {},
+  CRS.Simple,
+  {
+    /* overriding methods and members */
+    wrapLng: [MIN_SAFE_COORDINATES, MAX_SAFE_COORDINATES],
+    wrapLat: [MIN_SAFE_COORDINATES, MAX_SAFE_COORDINATES],
+    zoom(scale: number) {
+      Math.log(scale / BASE_SCALE) / Math.LN2;
+    },
+    scale(zoom: number) {
+      return BASE_SCALE * Math.pow(2, zoom);
+    },
+    projection: EvmLonLat,
+    transformation: transformation(1, 0, 1, 0),
+    infinite: false, // it's a torus : east connects with west and north with south
+
+    /* cutsom methods */
+
+    // Same as CRS.wrapLatLngBounds but doesn't keep the same size as the given one
+    // Useful for getting the visible bounds constrained into the CRS
+    constraintsLatLngBounds(bounds: LatLngBounds): LatLngBounds | null {
+      if (!bounds.intersects(WORLD_BOUNDS)) {
+        return null;
+      } else if (WORLD_BOUNDS.contains(bounds)) {
+        return bounds;
+      } else if (bounds.contains(WORLD_BOUNDS)) {
+        return WORLD_BOUNDS;
+      } else {
+        return new LatLngBounds(
+          CRS.Simple.wrapLatLng.call(this, bounds.getNorthEast()),
+          CRS.Simple.wrapLatLng.call(this, bounds.getSouthWest()),
+        );
+      }
+    },
   },
-  scale(zoom: number) {
-    return BASE_SCALE * Math.pow(2, zoom);
-  },
-  projection: EvmLonLat,
-  transformation: transformation(1, 0, 1, 0),
-  infinite: false, // it's a torus
-});
+);
