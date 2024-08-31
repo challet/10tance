4c707d17f4ab3e47df358034ab8469ee73b4bf40
Clément Hallet <clement@challet.eu>
2024-08-31 22:30:45 +0200

    use db connections pooling

--- packages/tileserver/src/sequelize/initDb.ts
+++ packages/tileserver/src/sequelize/initDb.ts
@@ -5,12 +5,22 @@ import { type Options, Sequelize } from 'sequelize';
 const CONFIG_BASE: Options = {
   port: 5432,
   dialectModule: pg,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: {
+    idle_in_transaction_session_timeout: 5000
+  },
+  pool: {
+    max: 2,
+    min: 0,
+    idle: 0,
+    acquire: 5000,
+    evict: 1000
+  }
 };
 
 export default async(): Promise<Sequelize> => {
-  const db = "POSTGRES_URL_NON_POOLING" in process.env 
-    ? new Sequelize(process.env.POSTGRES_URL_NON_POOLING as string, CONFIG_BASE)
+  const db = "POSTGRES_URL" in process.env 
+    ? new Sequelize(process.env.POSTGRES_URL as string, CONFIG_BASE)
     : new Sequelize({
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
